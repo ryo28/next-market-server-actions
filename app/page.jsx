e66b489e92e4eef5d80c9a2fcd88dc40319ccca5
ryo28 <wakisan1@ezweb.ne.jp>
2025-08-02 14:59:00 +0900

    新しいアイテム表示コンポーネントを追加し、データベースから全アイテムを取得する機能を実装

---
+++ app/page.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import connectDB from "./utils/database";
+import { itemModel } from "./utils/schemaModels";
+import Image from "next/image";
+
+const itemReadAll = async () => {
+  await connectDB(); //データベースに接続
+  const allItems = await itemModel.find(); //全てのアイテムを取得
+  return allItems;
+};
+
+export default async function ReadAllItems() {
+  const allItems = await itemReadAll();
+
+  return (
+    <div>
+      <h1>こんにちは</h1>
+      {allItems.map((item) => (
+        <Link href="" key={item._id}>
+          <Image
+            src={item.image}
+            width={750}
+            height={500}
+            alt="item-image"
+            priority
+          />
+          <div>
+            <h2>￥{item.price}</h2>
+            <h3>{item.name}</h3>
+            {/* substringを使用して、説明文の最初の80文字を表示 */}
+            <p>{item.description.substring(0, 80)}...</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
