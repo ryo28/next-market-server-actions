f1bd9abf910029fc504acf765857be66de2a1ed5
ryo28 <wakisan1@ezweb.ne.jp>
2025-08-02 15:00:13 +0900

    アイテム編集機能のための新しいページコンポーネントとフォームを追加

---
+++ app/item/update/[id]/page.jsx
@@ -0,0 +1,18 @@
+import { itemReadSingle } from "@/app/_lib/itemReadSingle";
+import { itemUpdate } from "@/app/actions/itemUpdate";
+import { EditItemForm } from "../_components/EditItemForm";
+
+export default async function UpdateItem({ params }) {
+  const { id } = await params;
+  const singleItem = await itemReadSingle(id); // IDに基づいてアイテムを取得
+  const itemUpdateWithId = itemUpdate.bind(null, id); // itemUpdate関数にIDをバインド
+  return (
+    <div>
+      <h1>アイテム編集</h1>
+      <EditItemForm
+        itemUpdateWithId={itemUpdateWithId}
+        singleItem={singleItem}
+      />
+    </div>
+  );
+}
---
+++ app/item/update/_components/EditItemForm.jsx
@@ -0,0 +1,35 @@
+export function EditItemForm({ itemUpdateWithId, singleItem }) {
+  return (
+    <form action={itemUpdateWithId} method="POST">
+      <input
+        defaultValue={singleItem.title}
+        type="text"
+        name="title"
+        placeholder="アイテム名"
+        required
+      />
+      <input
+        defaultValue={singleItem.price}
+        type="text"
+        name="price"
+        placeholder="価格"
+        required
+      />
+      <input
+        defaultValue={singleItem.image}
+        type="text"
+        name="image"
+        placeholder="画像"
+        required
+      />
+      <textarea
+        name="description"
+        rows={15}
+        defaultValue={singleItem.description}
+        placeholder="商品説明"
+        required
+      />
+      <button>編集</button>
+    </form>
+  );
+}
