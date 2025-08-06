932949986f7a61fd338877c78116bd0916e8c8f7
ryo28 <wakisan1@ezweb.ne.jp>
2025-08-06 19:50:41 +0900

    EditItemFormコンポーネントのdefaultValueを修正し、singleItemが存在しない場合の安全性を向上

--- app/item/update/_components/EditItemForm.jsx
+++ app/item/update/_components/EditItemForm.jsx
@@ -2,21 +2,21 @@ export function EditItemForm({ itemFormActions, singleItem, buttonText }) {
   return (
     <form action={itemFormActions}>
       <input
-        defaultValue={singleItem.title}
+        defaultValue={singleItem ? singleItem.title : ""}
         type="text"
         name="title"
         placeholder="アイテム名"
         required
       />
       <input
-        defaultValue={singleItem.price}
+        defaultValue={singleItem ? singleItem.price : ""}
         type="text"
         name="price"
         placeholder="価格"
         required
       />
       <input
-        defaultValue={singleItem.image}
+        defaultValue={singleItem ? singleItem.image : ""}
         type="text"
         name="image"
         placeholder="画像"
@@ -25,7 +25,7 @@ export function EditItemForm({ itemFormActions, singleItem, buttonText }) {
       <textarea
         name="description"
         rows={15}
-        defaultValue={singleItem.description}
+        defaultValue={singleItem ? singleItem.description : ""}
         placeholder="商品説明"
         required
       />
