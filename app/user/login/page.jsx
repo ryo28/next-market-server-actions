75c0d370cb1ddb9ee3ea535c09c8f1dc0ce28b4c
ryo28 <wakisan1@ezweb.ne.jp>
2025-08-07 17:38:55 +0900

    Loginコンポーネントを新規作成し、ユーザーのログイン機能を実装

---
+++ app/user/login/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { userLogin } from "@/app/actions/userLogin";
+import { useActionState } from "react";
+
+const initialState = {
+  message: "",
+};
+
+export default function Login() {
+  const [state, formAction] = useActionState(userLogin, initialState);
+  return (
+    <div>
+      <h1>ユーザーログイン</h1>
+      <form action={formAction}>
+        <label>
+          <input
+            type="email"
+            name="email"
+            placeholder="メールアドレス"
+            required
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            name="password"
+            placeholder="パスワード"
+            required
+          />
+        </label>
+        {state && <h3>{state.message}</h3>}
+        <button type="submit">ログイン</button>
+      </form>
+    </div>
+  );
+}
